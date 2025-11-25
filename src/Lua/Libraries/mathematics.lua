3ab99d57a1c8fd88400afd764bcecdc445becc59
Yanzari <srb2.workshop.yanzari@gmail.com>
2025-11-25 18:41:35 -0300

    From now on, the repository will be synchronized with my device.

--- src/Lua/Libraries/mathematics.lua
+++ src/Lua/Libraries/mathematics.lua
@@ -27,5 +27,41 @@ Math:FixedACos = function(fixed)
 return acos(fixed/FRACUNIT)*FRACUNIT
 end
 
+----~ returns the smaller fixed value between v1 and v2.
+--^ returns a fixed value
+Math:FixedMin = function(v1, v2)
+return min(v1/FRACUNIT,v2/FRACUNIT)*FRACUNIT
+end
+
+----~ returns the larger fixed value between v1 and v2.
+--^ returns a fixed value
+Math:FixedMax = function(v1, v2)
+return max(v1/FRACUNIT,v2/FRACUNIT)*FRACUNIT
+end
+
+----~ returns the larger fixed value between v1 and v2.
+--^ returns a fixed value
+Math:FixedPow = function(v1, v2)
+return max(v1/FRACUNIT,v2/FRACUNIT)*FRACUNIT
+end
+
+----~ returns the absolute value of v1
+--^ returns a fixed value
+Math:FixedAbs = function(v1)
+return abs(v1/FRACUNIT)*FRACUNIT
+end
+
+----~ returns the sign of v1
+--^ returns a fixed value
+Math:FixedSign = function(v1)
+local sign = 0
+if v1~=nil and type(v1) == "number" and v1 >= 0
+sign = 1
+elseif v1~=nil and type(v1) == "number" and v1 < 0
+sign = -1
+end
+return sign
+end
+
 ----~ returns to itself
 return Math
