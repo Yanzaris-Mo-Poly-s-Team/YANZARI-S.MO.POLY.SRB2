d9da599b499f8c9b8c18a0b258b360e46fe1b43f
Yanzari <srb2.workshop.yanzari@gmail.com>
2025-11-26 16:52:44 -0300

    Automatic change made in 2025-11-26 16:52

--- src/Lua/Scripts/Initialize/Hooks.lua
+++ src/Lua/Scripts/Initialize/Hooks.lua
@@ -1,9 +1,11 @@
 ----* Yanzari's Mo Poly
----* Some main functions
+---* Only hook functions.
 ---* By Yanzari
 local YMP = YanzMoPoly
-
 local Hooks = {}
+
+----~ activates a hook.
+--^ returns nil.
 function YMP:EnableHook(custom,hook)
 if not hook then return end
 if not custom then return end
@@ -11,6 +13,17 @@ if not (Hooks[hook][custom] and Hooks[hook][custom]) then return end
 Hooks[hook][custom].enabled = true
 end
 
+----~ Returns whether the hook is enabled or not.
+--^ returns boolean.
+function YMP:EnabledHook(custom,hook)
+if not hook then return false end
+if not custom then return false end
+if not (Hooks[hook][custom] and Hooks[hook][custom]) then return false end
+return Hooks[hook][custom].enabled
+end
+
+----~ Disables a hook.
+--^ returns nil.
 function YMP:DisableHook(custom,hook)
 if not hook then return end
 if not custom then return end
@@ -18,6 +31,8 @@ if not (Hooks[hook][custom] and Hooks[hook][custom]) then return end
 Hooks[hook][custom].enabled = false
 end
 
+----~ Delete a hook.
+--^ returns nil.
 function YMP:DeleteHook(custom,hook)
 if not hook then return end
 if not custom then return end
@@ -25,6 +40,8 @@ if not (Hooks[hook][custom] and Hooks[hook][custom]) then return end
 Hooks[hook][custom] = nil
 end
 
+----~ Delete a hook.
+--^ returns a hook_t.
 function YMP:AddHook(custom,hook,func,extra)
 if not hook then return end
 if not custom then return end
@@ -44,4 +61,5 @@ if Hooks[hook][custom] and Hooks[hook][custom].enabled == true
 func(hook,...)
 end)
 end
+return Hooks[hook][custom]
 end
